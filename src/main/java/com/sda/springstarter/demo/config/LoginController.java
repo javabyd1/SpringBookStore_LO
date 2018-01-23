224a323eee4c57f4080a9c237e336d6a283a8c3a
lukaszogn <lukaszogan@mat.umk.pl>
2018-01-23 21:02:12 +0100

    final

--- src/main/java/com/sda/springstarter/demo/config/LoginController.java
+++ src/main/java/com/sda/springstarter/demo/config/LoginController.java
@@ -3,6 +3,8 @@ package com.sda.springstarter.demo.config;
 import com.sda.springstarter.demo.model.User;
 import com.sda.springstarter.demo.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,6 +52,21 @@ public class LoginController {
             userService.saveUser(user);
             modelAndView.addObject("successMessage", "Zarejestrowales sie!");
         }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/admin/home", method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = userService.findUserByEmail(authentication.getName());
+
+        modelAndView.addObject("userName",
+                "Witaj: " + user.getEmail());
+
+        modelAndView.setViewName("admin/home");
         return modelAndView;
     }
 
